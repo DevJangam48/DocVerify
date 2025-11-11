6bce23fdea991b24c777de605f71da11445e7669
Devjangam48 <djangam4805@gmail.comgit config --global user.name DevJangam48>
2025-11-11 10:52:40 +0530

    Handled cors for future deployment

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,13 +1,24 @@
 const cors = require("cors");
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  process.env.CLIENT_URL, // The future URL of your deployed Amplify frontend
+];
+
 module.exports = (options = {}) => {
   const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: options.allowedMethods || ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: options.allowedHeaders || ["Content-Type", "Authorization"],
   };
 
   const corsMiddleware = cors(corsOptions);
-
   return corsMiddleware;
 };
